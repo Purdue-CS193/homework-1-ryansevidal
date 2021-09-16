055e06e8e95f076e0f370c51b096727db526e0c3
Ryan Alexander Sevidal <rsevidal@data.cs.purdue.edu>
2021-09-16 15:36:53 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,43 @@
 cd ORIGIN
 
 # Task 1 
-
+mv ~/HW2.txt ~/HW1.txt
 
 # Task 2
-
+rm -r ~/Memes/Spongebob/money.jpeg
 
 # Task 3
-
+mv ~/Memes/"Star Wars"/fafsa.jpg ~/Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rm -r ~/Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv ~/Programs/Age.cpp ~/Programs/C++/Age.cpp
 
 # Task 7
-
+cp ~/Programs/Java ~/Homework/CS180/Project_1
 
 # Task 8
-
+mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Programs/C
 
 # Task 9
-
+mv ~/Programs/Name.py ~/Programs/Space.py ~Programs/"Python 3"
 
 # Task 10
-
+touch ~/Homework/POL141/Essay.txt
 
 # Task 11
-
+ls -a
 
 # Task 12
 : '
 Type your vim/nano customizations below:
-
-
+:set number
+:set autoindent expandtab
+:set mouse=a
 
 '
